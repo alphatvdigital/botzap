4dc9e3af4519bb30317aedba07ce7ded87acbd65
alphatvdigital <alphatvdigital@gmail.com>
2025-05-09 10:48:17 -0300

    Ajusta porta padrão para 8080

--- app.py
+++ app.py
@@ -75,5 +75,5 @@ def webhook():
     return "OK", 200
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 8080))  # altere 5000 → 8080
     app.run(host="0.0.0.0", port=port)
